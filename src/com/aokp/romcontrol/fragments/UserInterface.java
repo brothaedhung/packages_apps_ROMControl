a8a27a02a18efc0ee2e5526e2a0ead26c308d667
Matanya Elchanani <melchanani@hotmail.com>
2013-01-25 12:50:29 -0500

    RC: fix custom boot animation
    The custom boot animation had a race condition that ended up
    changing the path to always be the system boot animation. Using
    a local version of the path and setting it during the pre-execute
    stage solves the race.
    
    Change-Id: I341a131d05a748dfdc1205e488ba19fcc8d1a46a

--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -789,6 +789,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
 
     class InstallBootAnimTask extends AsyncTask<Void, Void, Void> {
         private final DialogInterface dialog;
+        private String taskAnimationPath;
 
         public InstallBootAnimTask(DialogInterface dialog) {
             this.dialog = dialog;
@@ -796,6 +797,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
 
         protected void onPreExecute() {
             //Update setting to reflect that boot animation is now enabled
+            taskAnimationPath = mBootAnimationPath;
             mDisableBootAnimation.setChecked(false);
             DisableBootAnimation();
             dialog.dismiss();
@@ -804,7 +806,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
         @Override
         protected Void doInBackground(Void... voids) {
             //Copy new bootanimation, give proper permissions
-            new CMDProcessor().su.runWaitFor("cp "+ mBootAnimationPath +" /data/local/bootanimation.zip");
+            new CMDProcessor().su.runWaitFor("cp "+ taskAnimationPath +" /data/local/bootanimation.zip");
             new CMDProcessor().su.runWaitFor("chmod 644 /data/local/bootanimation.zip");
             return null;
         }
